5991a440c346d29fdbbd4f335c1f10e1f4e869fa
Chris Simpkins <git.simpkins@gmail.com>
2021-05-05 15:39:13 -0400

    clean out all previous Platypus based sources and docs

--- dmg-builder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-dmgbuild -s settings.py -D app=Woffle.app "Woffle" Woffle-Installer.dmg
-
-if [ -f Woffle-Installer.dmg ]; then
-	mv Woffle-Installer.dmg installer/Woffle-Installer.dmg
-fi
